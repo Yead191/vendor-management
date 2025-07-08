0270a4e57dce7906566765d9eaff0c3002196cd1
Yead191 <yead191@gmail.com>
2025-07-08 15:22:31 +0600

    remove dark mode

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { useRouter } from "next/navigation";
-import React from "react";
 import { toast } from "sonner";
-
 
 export default function Home() {
   const user = null;
   const router = useRouter();
+
   if (!user) {
     toast.error("Please Login to continue...");
     return router.push("/auth/login");
